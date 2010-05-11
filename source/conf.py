50bbb9242e1b1024a23c85f1fa780c6786e0ecfe
Atamert Ölçgen <muhuk@muhuk.com>
2010-05-11 18:23:58 +0300

    Changed copyright

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'contents'
 
 # General information about the project.
 project = u'pyist.net'
-copyright = u'2010, Atamert Ölçgen'
+copyright = u'2010, PYist'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
